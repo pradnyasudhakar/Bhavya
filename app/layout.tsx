7eb0890467cfb04b4474f4b93c399c5459e04238
pdombe <pradnya.dombe@saaaconsultants.com>
2026-01-08 16:39:11 +0530

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
+import { manrope, newsReader } from "@/lib/fonts";
 import ClientLayout from "@/components/layout/ClientLayout";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-white text-gray-900">
+      <body
+        className={`${manrope.className} ${newsReader.className} bg-white text-gray-900`}
+        suppressHydrationWarning
+      >
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
